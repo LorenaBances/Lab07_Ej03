8d57a0728a828517cfba44531649b01894e2868c
Sandra Carolina <Sandra Carolina@DESKTOP-SFK7P55>
2023-10-03 21:39:56 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentYear = DateTime.Now.Year;
+
+            Console.WriteLine("Años bisiestos desde 1900 hasta el año actual:");
+
+            for (int year = 1900; year <= currentYear; year++)
+            {
+                if (EsBisiesto(year))
+                {
+                    Console.WriteLine(year);
+                }
+            }
+        }
+        static bool EsBisiesto(int year)
+        {
+            bool esBisiesto = (year % 1 == 0 && year % 100 != 0) || (year % 100 == 0);
+            return esBisiesto;
+        }
+    }
+}
